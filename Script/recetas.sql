b7fa1a2ed38fe79f028bda6b61dc22423f201684
Felipe01210 <erlipe16@gmail.com>
2023-09-19 13:35:24 +0200

    Casi acabao

---
+++ Script/recetas.sql
@@ -0,0 +1,38 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+create database Recetas;
+
+use Recetas;
+
+grant all privileges on Recetas.* to 'felipe'@'%' identified by 'felipe';
+
+create table cocinero(
+    nombre varchar(30),
+    apellidos varchar(30),
+    email varchar(30),
+    password varchar(30),
+    estrellas INT,
+
+    constraint pk_cocinero primary key (email)
+)
+
+create table receta(
+    titulo varchar(100),
+    descripcion varchar(500),
+    nota real,
+    cocinero varchar(30),
+
+    constraint pk_receta primary key (titulo),
+    constraint fk_receta foreign key (cocinero) references cocinero(nombre)
+)
+
+create table historial(
+    nombre varchar(30),
+    titulo varchar(100),
+
+    constraint pk_historial primary key (nombre,titulo),
+    constraint fk_his_cocinero foreign key (nombre) references cocinero(nombre),
+    constraint fk_his_receta foreign key (titulo) references receta(titulo)
+)
--- index.js
+++ index.js
@@ -23,16 +23,76 @@ for (let imagen of imagenes) {
     });
 }
 
-let opinion = document.querySelector("textarea");
+let opinion_texto = document.querySelector("textarea");
 let nombre = document.querySelectorAll("input")[0];
 let sat = document.querySelectorAll("input")[1];
 
+let small = document.querySelector("small");
+
 let enviar = document.getElementById("enviar");
 
-opinion.addEventListener("input",() => {
-    if()
-    enviar.removeAttribute("hidden")
+nombre.addEventListener("input",() => {
+    if(opinion_texto.value.length > 0 && sat.value.length > 0){
+        small.textContent = "";
+        small.removeAttribute("class");
+        enviar.removeAttribute("hidden")
+    }
+})
+
+sat.addEventListener("input",() => {
+    if(opinion_texto.value.length > 0 && nombre.value.length > 0){
+        small.textContent = "";
+        small.removeAttribute("class");
+        enviar.removeAttribute("hidden")
+    }
+})
+
+opinion_texto.addEventListener("input",() => {
+    console.log(nombre.value);
+    console.log(sat.value);
+    if(nombre.value.length <= 0){
+        small.setAttribute("class","error");
+        small.textContent = "Nombre obligatorio";
+    }
+    if(sat.value < 0 || sat.value > 10){
+        small.textContent = "La valoración debe estar entre 0 y 10";
+        small.setAttribute("class","error");
+    }else if(nombre.value.length > 2 && sat.value.length > 0){
+        small.textContent = "";
+        small.removeAttribute("class");
+        enviar.removeAttribute("hidden")
+    }
 });
+
+sat.addEventListener("input",() => {
+    if(sat.value > 10){
+        sat.value = sat.value % 10;
+    }else if(sat.value < 0){
+        sat.value = 0;
+    }
+})
+
+let opiniones = document.querySelector(".opiniones")
+
+enviar.addEventListener("click", () => {
+    let opinion = document.createElement("div");
+    opinion.setAttribute("class", "opinion");
+    let op_nombre = document.createElement("h4");
+    op_nombre.textContent = nombre.value + " " + sat.value;
+
+    let op_texto = document.createElement("p");
+    op_texto.textContent = opinion_texto.value;
+
+    opinion.appendChild(op_nombre);
+    opinion.appendChild(op_texto);
+    opiniones.appendChild(opinion);
+
+    nombre.value = "";
+    sat.value = 0;
+    opinion_texto.value = "";
+
+    enviar.setAttribute("hidden","true")
+})
 
 
 
